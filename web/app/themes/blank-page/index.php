a1d91e37a4c18220a618246b4771c191505e1919
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-12-12 18:35:04 +0100

    Minor styling on theme's index.php

--- web/app/themes/blank-page/index.php
+++ web/app/themes/blank-page/index.php
@@ -6,10 +6,22 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>kodefant.no</title>
+	<style>
+		body {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			height: 100vh;
+			width: 100%;
+			font-size: 1.5rem;
+		}
+	</style>
 </head>
 
 <body>
-	Ute etter en webutvikler? <a href="https://kodefant.no">Besøk kodefant.no</a>
+	<p>Leter du etter en dyktig webutvikler?</p>
+	<p><a href="https://kodefant.no">Gå til kodefant.no</a></p>
 
 </body>
 
