7868ca2b5b0c009671710c2ae0f15b13d3af0be2
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-12-12 18:16:17 +0100

    Added a blank theme, ACF and CPT UI

---
+++ web/app/themes/blank-page/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>kodefant.no</title>
+</head>
+
+<body>
+	Ute etter en webutvikler? <a href="https://kodefant.no">Besøk kodefant.no</a>
+
+</body>
+
+</html>
